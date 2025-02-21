5a1893e85bd7fda07f0a0689c7b3ff3f0812a277
Samaysrivastava <samay8193@gmail.com>
2025-02-22 00:22:46 +0530

    objects  in js

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,43 @@
+// singleton object->jb constructor se bnate hai tb singleton bnta hai
+// Object.create(); // constructor singleton bnta hai
+// onject literals
+const mySym = Symbol();
+const jsUser = {
+    // key: value
+    "name": "samay",
+    age:18,
+    location:"jaipur",
+    email:"samay@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["monday","tuesday"],
+    [mySym]:"my symbol key"
+};
+// myArray = ["samay","ranveer","ashish"];
+console.log(jsUser);
+console.log(jsUser["name"]);
+console.log(jsUser.email);
+console.log(jsUser["email"]); //correct way
+
+console.log(jsUser[mySym]); // correct way
+// way to change value
+jsUser.email="harsh@gmail.com";
+console.log(jsUser.email);
+
+// freeze object 
+// Object.freeze(jsUser);  // by this we cannot perform  changes on object
+jsUser.email="harsh@gmail.com";
+console.log(jsUser.email);
+
+
+jsUser.greeting = function(){
+    console.log("hello js user");
+}
+jsUser.greeting2 = function(){
+    console.log(`hello js user, ${this.name}`);
+}
+// console.log(jsUser.greeting); //return function
+console.log(jsUser.greeting2()); 
+
+
+console.log(jsUser);
+
